0cbd8c1109a86935d73fe7073a8a8315f33f2ff1
Josue <josgar_14@hotmail.com>
2026-02-21 01:48:37 -0400

    my first commit

---
+++ src/components/Scroll.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Scroll = (props) => {
+  return (
+    <div
+      style={{ overflow: "scroll", border: "5px solid green", height: "800px" }}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export default Scroll;
